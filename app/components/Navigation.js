149c81ab4d4f874bae62dc1f75d5d929d0d83663
Manuel Sucaria <manusucaria@gmail.com>
2023-03-18 11:27:51 -0300

    18 del 3 1

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -5,73 +5,39 @@ import fotoPortada from "../assets/img/logo-negro.png";
 import Link from "next/link";
 
 export default function Header() {
-    const [isNavOpen, setIsNavOpen] = useState(false);
+    const [isNavOpen, setIsNavOpen] = useState(true);
     return (
-        <nav className="flex items-center bg-amber-50">
-            <div className="pl-4 space-y-2 w-1/6 me-auto" onClick={() => setIsNavOpen((prev) => !prev)}>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            </div>
-            <section>
-                <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-                    <div
-                    className="absolute top-0 right-0 px-8 py-8"
-                    onClick={() => setIsNavOpen(false)}
-                    >
-                        <svg
-                            className="h-8 w-8 text-gray-600"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
+        <nav className="bg-amber-50 flex flex-col">
+            <div className="flex items-center">
+                <div className="flex flex-col w-1/6">
+                    <div className="pl-4 space-y-2" onClick={() => setIsNavOpen((prev) => !prev)}>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
                     </div>
-                    <ul className="flex flex-col items-center justify-between min-h-[250px]">
-                        <li className="border-b border-gray-400 my-8 uppercase">
+                </div>
+                <div className="flex w-4/6 mr-auto">
+                    <Link className="flex h-24 w-24 mx-auto" href="/" onClick={() => setIsNavOpen(true)}>
+                        <Image className="my-auto" src={fotoPortada} alt="logo" priority />
+                    </Link>
+                </div>
+            </div>
+            {isNavOpen === false ?
+                    <ul className="flex flex-col traslate-neg h-auto mb-2">
+                        <li className="my-2 ml-4 uppercase">
                             <Link onClick={() => setIsNavOpen(false)} href="/actividades">Actividades</Link>
                         </li>
-                        <li className="border-b border-gray-400 my-8 uppercase">
+                        <li className="my-2 ml-4 uppercase">
                             <Link onClick={() => setIsNavOpen(false)} href="/nosotros">Nosotros</Link>
                         </li>
-                        <li className="border-b border-gray-400 my-8 uppercase">
+                        <li className="my-2 ml-4 uppercase">
                             <Link onClick={() => setIsNavOpen(false)} href="/ayuda">Contacto</Link>
                         </li>
-                        <li className="border-b border-gray-400 my-8 uppercase">
+                        <li className="my-2 ml-4 uppercase">
                             <Link onClick={() => setIsNavOpen(false)} href="/directorio">Directorio</Link>
                         </li>
                     </ul>
-                </div>
-            </section>
-            <div className="w-4/6 flex">
-                <Link className="flex h-24 w-24 mx-auto" href="/">
-                    <Image className="my-auto" src={fotoPortada} alt="logo" priority />
-                </Link>
-            </div>
-            <style>{`
-            .hideMenuNav {
-                display: none;
-            }
-            .showMenuNav {
-                display: block;
-                position: absolute;
-                width: 100%;
-                height: 100vh;
-                top: 0;
-                left: 0;
-                background: white;
-                z-index: 10;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-evenly;
-                align-items: center;
-            }
-            `}</style>
+                    : ""}
         </nav>
     );
 }

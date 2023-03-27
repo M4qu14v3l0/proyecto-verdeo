7268d0d92dad4c9c59691c6b05225d4a149af76b
Manuel Sucaria <manusucaria@gmail.com>
2023-03-27 13:00:32 -0300

    27 del 3 2

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
 'use client';
-import {useEffect} from 'react';
-import { getArticle } from './api/api';
 import Actividades from './components/Actividades.js';
 import Nosotros from './components/Nosotros.js';
 import NosotrosFull from './components/NosotrosFull';
@@ -11,11 +9,6 @@ import AyudaFull from './components/AyudaFull';
 import Hero from './components/Hero';
 
 export default function Home() {
-  useEffect(() => {
-      getArticle().then(data => {
-          setArticulos(data)
-      })
-  }, []);
   return (
     <main className="flex flex-col pb-6">
       <Hero />
@@ -26,17 +19,6 @@ export default function Home() {
       <Ayuda />
       <DirectorioFull />
       <Directorio />
-      {/* {articulos.length >= 1 ? 
-        <ul className="flex gap-7 mb-2 mx-auto">                        
-          {articulos.map((articulo) => (
-              <div key={articulo.titulo}>
-                  <h4>{articulo.titulo}</h4>
-                  <p>{articulo.cuerpo}</p>
-                  <p>{articulo.img}</p>
-              </div>
-          ))}
-      </ul>
-      : ""} */}
     </main>
   )
 }

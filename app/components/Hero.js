f63e449287e31c54171affaacaede0a30e29b5ec
Manuel Sucaria <manusucaria@gmail.com>
2023-04-11 13:12:33 -0300

    11 el 4 7

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -8,8 +8,8 @@ const Hero = () => {
                 <Link href='https://www.instagram.com/a_burninghill/' target="_blank" className="p-3 text-xs hidden lg:inline font-botones ml-auto mt-auto text-blanco">@a_burninghill</Link>
             </div>
             <div className='mt-auto text-center col-start-1 col-end-1 row-start-1 row-end-1 flex flex-col'>
-                <h1 className='mb-8 lg:hidden text-blanco font-titulos font-bold m-auto text-3xl md:text-5xl'>Proyecto <br/> Verdeo</h1>
-                <p className="p-3 text-xs lg:hidden font-botones ml-auto mt-auto text-blanco">@a_burninghill</p>
+                <h1 className='mb-8 lg:hidden text-blanco font-titulos font-bold m-auto text-5xl md:text-6xl'>Proyecto <br/> Verdeo</h1>
+                <Link href='https://www.instagram.com/a_burninghill/' target="_blank" className="p-3 text-xs lg:hidden font-botones ml-auto mt-auto text-blanco">@a_burninghill</Link>
             </div>
         </div>
     )

90a46a7e3e273b9ec822a57bf7d0ebed97b6b9a2
Manuel Sucaria <manusucaria@gmail.com>
2023-04-14 11:00:47 -0300

    14 del 4 10

--- app/nosotros/components/nuestrosValores/NuestrosValores.jsx
+++ app/nosotros/components/nuestrosValores/NuestrosValores.jsx
@@ -18,7 +18,7 @@ export default function NuestrosValores() {
               />
             </div>
             <div className="h-[300px] sm:h-[400px] lg:p-28 lg:h-[500px] xl:h-[600px] 2xl:h-[700px] w-full bg-[#F4B847] flex flex-col justify-center items-center">
-              <h1 className="font-bold text-black text-2xl lg:text-4xl font-titulos">Nuestros valores</h1>
+              <h1 className="font-bold text-2xl lg:text-4xl font-titulos">Nuestros valores</h1>
               <p className="font-botones p-5  lg:w-full text-sm md:text-base">Ser fieles a nuestras responsabilidades e ideales como integrantes de este proyecto y ciudadanos, es algo que buscamos a través de lo que hacemos.
                 Además, la humildad, la ética y la colaboración son valores importantes para este proyecto, ya que buscamos fomentar una participación activa y comprometida de la sociedad en la lucha por un futuro más sostenible y en armonía con todos los seres del planeta.
               </p>
